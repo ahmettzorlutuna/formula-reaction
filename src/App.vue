4a9647a9d6e4322a6e14bcf9ca983c6796375143
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-10-18 23:34:15 +0300

    Using block component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to reaction test!</h1>
+  <Block v-if="isWaiting" @click="start" style="cursor: pointer;"/>
+  <!-- <Block v-if="isReady" style="cursor: pointer;"/> -->
+  <Block v-if="isPlaying" :class="{ isPlaying: isPlaying}" :delay="delay" style="cursor: pointer;"/>
+
+  <!-- <Block :delay="delay" @click="start" :class="{ isPlaying: isPlaying}"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Block
+  },
+  methods: {
+    start(){
+      this.isPlaying= true
+      this.isWaiting= false
+      this.isReady = true
+      this.delay = 2000 + Math.random() * 3000; 
+      console.log(this.delay)
+    }
+  },
+  data() {
+    return {
+      isPlaying: false,
+      isWaiting: true,
+      isReady: false,
+      delay: null
+    }
+  },
 }
 </script>
 
